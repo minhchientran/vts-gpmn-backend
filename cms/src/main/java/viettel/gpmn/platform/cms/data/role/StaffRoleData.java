a03307779410b6de0ecd3d926e4b1ef9338c6f4b
chientm4 <chientm4@viettel.com.vn>
2024-07-10 09:38:41 +0700

    Add fromDate & toDate to StaffRoleData

--- cms/src/main/java/viettel/gpmn/platform/cms/data/role/StaffRoleData.java
+++ cms/src/main/java/viettel/gpmn/platform/cms/data/role/StaffRoleData.java
@@ -5,10 +5,14 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import viettel.gpmn.platform.core.data.BaseData;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class StaffRoleData extends BaseData {
     private String staffId;
     private String roleId;
+    private LocalDateTime fromDate;
+    private LocalDateTime toDate;
 }
