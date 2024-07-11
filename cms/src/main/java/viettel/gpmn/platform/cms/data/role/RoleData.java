510d6a1087c3702831c63e9dba90bb0665eef672
chientm4 <chientm4@viettel.com.vn>
2024-07-11 11:13:12 +0700

    Fix RoleData change String to LocalDateTime

--- cms/src/main/java/viettel/gpmn/platform/cms/data/role/RoleData.java
+++ cms/src/main/java/viettel/gpmn/platform/cms/data/role/RoleData.java
@@ -5,10 +5,12 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import viettel.gpmn.platform.core.data.InfoData;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class RoleData extends InfoData {
-    private String fromDate;
-    private String toDate;
+    private LocalDateTime fromDate;
+    private LocalDateTime toDate;
 }
